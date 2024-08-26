8559881280c9732adab062488e48ca939c39d4a2
gjc33 <97050192+gjc33@users.noreply.github.com>
2024-08-26 15:51:16 +0100

    code update

--- Singstar.ps1
+++ Singstar.ps1
@@ -13,14 +13,50 @@ $entryTemplate = @'
 
 $xml = @'
 <?xml version="1.0" encoding="UTF-8"?>
-<?xml-stylesheet type="text/xsl" href="./Singstar.xsl"?>
+<document>
+<!DOCTYPE document [ <!ATTLIST xsl:stylesheet id ID #REQUIRED> ]>
 <Catalogue>
 '@
+
+$stylesheet = @'
+<?xml version="1.0" encoding="UTF-8"?>
+
+<xsl:stylesheet version="1.0"
+xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+<xsl:template match="/">
+  <html>
+  <body>
+  <h2>Singstar Catalogue</h2>
+  <table border="1">
+    <tr bgcolor="#9acd32">
+      <th>Format</th>
+      <th>Disc</th>
+      <th>Artist</th>
+      <th>Song Title</th>
+    </tr>
+    <xsl:for-each select="Catalog/Track">
+    <tr>
+      <td><xsl:value-of select="Format"/></td>
+      <td><xsl:value-of select="Disc"/></td>
+      <td><xsl:value-of select="Artist"/></td>
+      <td><xsl:value-of select="SongTitle"/></td>
+    </tr>
+    </xsl:for-each>
+  </table>
+  </body>
+  </html>
+</xsl:template>
+
+</xsl:stylesheet>
+'@
+
+$xml += $stylesheet
 
 $xml += $data | ForEach-Object {
     $ExecutionContext.InvokeCommand.ExpandString($entrytemplate)
 }
 
-$xml += "</Catalogue>"
+$xml += "</Catalogue></document>"
 
 $xml | Out-File $(Join-Path $FolderPath "index.html")
