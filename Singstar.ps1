2f2b71e63a1c2bee0a555e840fd1bae8fa6e914a
gjc33 <97050192+gjc33@users.noreply.github.com>
2024-08-26 15:16:57 +0100

    Create repository

---
+++ Singstar.ps1
@@ -0,0 +1,26 @@
+﻿$FolderPath = "S:\Singstar\"
+
+$data = Import-Csv -Path $(Join-Path $Folderpath "Singstar.csv") -Delimiter ","
+
+$entryTemplate = @'
+<Track>
+    <Format>$($_.Format)</Format>
+    <Disc>$($_.Disc)</Disc>
+    <Artist>$($_.Artist)</Artist>
+    <SongTitle>$($_."Song Title")</SongTitle>
+</Track>
+'@
+
+$xml = @'
+<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="Singstar.xsl"?>
+<Catalogue>
+'@
+
+$xml += $data | ForEach-Object {
+    $ExecutionContext.InvokeCommand.ExpandString($entrytemplate)
+}
+
+$xml += "</Catalogue>"
+
+$xml | Out-File $(Join-Path $FolderPath "Singstar.XML")
