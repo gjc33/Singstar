64439c9454cbf5aa716b117db3f01fb9ada38bec
gjc33 <97050192+gjc33@users.noreply.github.com>
2024-08-26 16:20:36 +0100

    code

--- Singstar.ps1
+++ Singstar.ps1
@@ -36,7 +36,7 @@ $stylesheet = @'
       <th>Artist</th>
       <th>Song Title</th>
     </tr>
-    <xsl:for-each select="document/Catalog/Track">
+    <xsl:for-each select="document/Catalogue/Track">
     <tr>
       <td><xsl:value-of select="Format"/></td>
       <td><xsl:value-of select="Disc"/></td>
