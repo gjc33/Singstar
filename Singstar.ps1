f23027130b741dce5a4173ed2aa2df5ebd7a79c7
gjc33 <97050192+gjc33@users.noreply.github.com>
2024-08-26 17:32:15 +0100

    Code

--- Singstar.ps1
+++ Singstar.ps1
@@ -1,23 +1,10 @@
 ﻿$FolderPath = "C:\Users\garyc\Documents\GitHub\Singstar\"
 
 $data = Import-Csv -Path $(Join-Path $Folderpath "Singstar.csv") -Delimiter ","
-
-$html = @'
-<html>
-    <body>
-  <h2>Singstar Catalogue</h2>
-      <table border="1">
-        <tr bgcolor="#9acd32">
-          <th>Format</th>
-          <th>Disc</th>
-          <th>Artist</th>
-          <th>Song Title</th>
-        </tr>
-'@
-
 $entryTemplate = @'
 
         <tr>
+            <td>$($i)</td>
             <td>$($_.Format)</td>
             <td>$($_.Disc)</td>
             <td>$($_.Artist)</td>
@@ -26,15 +13,30 @@ $entryTemplate = @'
 
 '@
 
-$html += $data | ForEach-Object {
+$html = @'
+<html>
+    <body>
+    <h2>Singstar Catalogue</h2>
+
+    <table border='1'>
+        <tr bgcolor='#9acd32'>
+            <th>Index</th>
+            <th>Format</th>
+            <th>Disc</th>
+            <th>Artist</th>
+            <th>Song Title</th></tr>
+'@
+
+$html += $data | Sort "Artist"| ForEach-Object {$i = 1}{
     $ExecutionContext.InvokeCommand.ExpandString($entrytemplate)
+    $i++
 }
 
 $html += @'
-
     </body>
 </html>
 '@
 
 
 $html | Out-File $(Join-Path $FolderPath "index.html")
+
